c1d426114ac52df26530a1eea9031f0fbad265ed
Thomas Fan <thomasjpfan@gmail.com>
2017-04-29 19:36:55 -0400

    Remove error message

--- plugins/liquid_tags/notebook.py
+++ plugins/liquid_tags/notebook.py
@@ -63,13 +63,6 @@ try:
 except:
     pass
 
-if not IPYTHON_VERSION >= 1:
-    raise ValueError("IPython version 1.0+ required for notebook tag")
-
-if IPYTHON_VERSION > 1:
-    warnings.warn("Pelican plugin is not designed to work with IPython "
-                  "versions greater than 1.x. CSS styles have changed in "
-                  "later releases.")
 
 try:
     from nbconvert.filters.highlight import _pygments_highlight
