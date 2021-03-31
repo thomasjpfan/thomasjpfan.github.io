c54ef91f9d3d1ae3e887d9bdfe8360a6c16bc9ea
Thomas J. Fan <thomasjpfan@gmail.com>
2021-03-31 18:39:29 -0400

    BLD Set access control

--- publishconf.py
+++ publishconf.py
@@ -12,7 +12,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://thomasjpfan.com"
+SITEURL = "https://www.thomasjpfan.com"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
@@ -24,4 +24,3 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # DISQUS_SITENAME = ""
 # GOOGLE_ANALYTICS = ""
-
